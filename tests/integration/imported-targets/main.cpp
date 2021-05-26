f52a7dd9d55cbf64ce85ba585e2aa2458945ce9f
Robert Walton <rob.walton@arm.com>
2021-05-26 14:18:18 +0100

    ci: Add integration test for exported targets
    Add a dummy application using find_package to discover greentea-client
    as an integration test for exported CMake targets.

---
+++ tests/integration/imported-targets/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "greentea-client/test_env.h"
+
+int main()
+{
+    static const char *const key = "Hello";
+    static const char *const val = "99";
+    greentea_send_kv(key, val);
+
+    return 0;
+}
